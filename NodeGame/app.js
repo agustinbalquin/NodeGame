78e83184afccb41a69f90915f8524b00cba8c7d4
agustinbalquin <agustinbalquin@gmail.com>
2016-03-26 18:08:46 -0700

    Added engine and index scripts

--- NodeGame/app.js
+++ NodeGame/app.js
@@ -5,11 +5,11 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
  
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); // sets public as the home directory
  
 app.get('/', function(req, res){
   res.render('/index.html');
 });
  
-server.listen(80);
+server.listen(80); // listen on port 80
 console.log("Multiplayer app listening on port 80");
